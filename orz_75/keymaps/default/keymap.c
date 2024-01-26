8bffde87ec98ebc56c24642e9bf745384648f207
居戎氏 <chen.sst@gmail.com>
2024-01-26 22:57:26 +0800

    囧 75: 利用重复键位

--- orz_75/keymaps/default/keymap.c
+++ orz_75/keymaps/default/keymap.c
@@ -7,30 +7,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
      * │   1  │   2  │   3  │   4  │   5  │   `  │   6  │   7  │   8  │   9  │   0  │ Bksp │
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │   Q  │   W  │   E  │   R  │   T  │  Tab │   Y  │   U  │   I  │   O  │   P  │ Enter│
+     * │  Tab │   Q  │   W  │   E  │   R  │   T  │   Y  │   U  │   I  │   O  │   P  │ Enter│
      * ├───┼───┼───┼───┼───┤      ├───┼───┼───┼───┼───┤      │
-     * │   A  │   S  │   D  │   F  │   G  │      │   H  │   J  │   K  │   L  │   ;  │      │
+     * │  Esc │   A  │   S  │   D  │   F  │   G  │   H  │   J  │   K  │   L  │   ;  │      │
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │   Z  │   X  │   C  │   V  │   B  │ Shift│   N  │   M  │   ,  │   .  │   /  │ Shift│
+     * │ Shift│   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │ Shift│
      * ├───┴───┼───┼───┼───┤      ├───┴───┼───┼───┼───┤      │
-     * │  Ctrl / Esc  │  Alt │  Gui │  Fn  │      │     space    │  Gui │  Alt │ Ctrl │      │
+     * │      │  Ctrl│  Alt │  Gui │  Fn  │ space│ space│  Fn  │ Left │ Down │  Up  │ Right│
      * └───────┴───┴───┴───┴───┴───────┴───┴───┴───┴───┘
      */
     [0] = LAYOUT_36_36(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        LCTL_T(KC_ESC),   KC_LALT, KC_LGUI, MO(1),            KC_SPC,           KC_RGUI, KC_RALT, KC_RCTL
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+        MO(2),   KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
     [1] = LAYOUT_36_36(
+        KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+        KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS,
-        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
 };
--- orz_75/orz_75.h
+++ orz_75/orz_75.h
@@ -8,16 +8,16 @@
   k3H, k3G, k3F, k3E, k3D, k3C, k6C, k6D, k6E, k6F, k6G, k6H, \
   k2H, k2G, k2F, k2E, k2D, k2C, k7C, k7D, k7E, k7F, k7G, k7H, \
   k1H, k1G, k1F, k1E, k1D, k1C, k8C, k8D, k8E, k8F, k8G, k8H, \
-  k1I, k4I, k8I, k6J, k5J,      k8B, k5B, k1B, k3A, k4A,      \
+  k1I, k4I, k8I, k6J, k5J, k4C, k8B, k5B, k1B, k3A, k4A, k5H, \
   k2I, k6I, k7I, k4J, k1J, k2J, k7B, k3B, k2B, k5A, k8A, k7A, \
-    k5I,    k7J, k8J, k3J,        k4B,    k2A, k1A, k6A       \
+  k3I, k5I, k7J, k8J, k3J, k4H, k6B, k4B, k2A, k1A, k6A, k5C \
 ) { \
   {k1A, k1B, k1C, k1D, k1E, k1F, k1G, k1H, k1I, k1J}, \
   {k2A, k2B, k2C, k2D, k2E, k2F, k2G, k2H, k2I, k2J}, \
-  {k3A, k3B, k3C, k3D, k3E, k3F, k3G, k3H, XXX, k3J}, \
-  {k4A, k4B, XXX, XXX, XXX, XXX, XXX, XXX, k4I, k4J}, \
-  {k5A, k5B, XXX, XXX, XXX, XXX, XXX, XXX, k5I, k5J}, \
-  {k6A, XXX, k6C, k6D, k6E, k6F, k6G, k6H, k6I, k6J}, \
+  {k3A, k3B, k3C, k3D, k3E, k3F, k3G, k3H, k3I, k3J}, \
+  {k4A, k4B, k4C, XXX, XXX, XXX, XXX, k4H, k4I, k4J}, \
+  {k5A, k5B, k5C, XXX, XXX, XXX, XXX, k5H, k5I, k5J}, \
+  {k6A, k6B, k6C, k6D, k6E, k6F, k6G, k6H, k6I, k6J}, \
   {k7A, k7B, k7C, k7D, k7E, k7F, k7G, k7H, k7I, k7J}, \
   {k8A, k8B, k8C, k8D, k8E, k8F, k8G, k8H, k8I, k8J}, \
 }

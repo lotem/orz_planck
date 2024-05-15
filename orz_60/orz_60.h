d47a4f0b921534bd39053bf66dc71d7ebf33a444
居戎氏 <chen.sst@gmail.com>
2024-05-16 00:53:17 +0800

    (orz_60): 调整矩阵
    升级为正式主控板，用洞洞板制作。
    行序统一为轴板上标注的顺序。

--- orz_60/orz_60.h
+++ orz_60/orz_60.h
@@ -5,11 +5,11 @@
 #define XXX KC_NO
 
 #define LAYOUT_5x12( \
-  k1N, k1M, k1L, k1K, k1J, k1I, k1C, k1D, k1E, k1F, k1G, k1H, \
-  k2N, k2M, k2L, k2K, k2J, k2I, k2C, k2D, k2E, k2F, k2G, k2H, \
+  k5N, k5M, k5L, k5K, k5J, k5I, k1C, k1D, k1E, k1F, k1G, k1H, \
+  k4N, k4M, k4L, k4K, k4J, k4I, k2C, k2D, k2E, k2F, k2G, k2H, \
   k3N, k3M, k3L, k3K, k3J, k3I, k3C, k3D, k3E, k3F, k3G, k3H, \
-  k4N, k4M, k4L, k4K, k4J, k4I, k4C, k4D, k4E, k4F, k4G, k4H, \
-  k5N, k5M, k5L, k5K, k5J, k5I, k5C, k5D, k5E, k5F, k5G, k5H \
+  k2N, k2M, k2L, k2K, k2J, k2I, k4C, k4D, k4E, k4F, k4G, k4H, \
+  k1N, k1M, k1L, k1K, k1J, k1I, k5C, k5D, k5E, k5F, k5G, k5H \
 ) { \
   {k1C, k1D, k1E, k1F, k1G, k1H, k1I, k1J, k1K, k1L, k1M, k1N}, \
   {k2C, k2D, k2E, k2F, k2G, k2H, k2I, k2J, k2K, k2L, k2M, k2N}, \
